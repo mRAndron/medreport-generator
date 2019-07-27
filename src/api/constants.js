05a5d3d565f743224083337d210ad55d1b1a6856
lulldev <lulldev@yandex.ru>
2019-07-27 11:21:35 +0300

    add postinstall, move tag-example to public

--- src/api/constants.js
+++ src/api/constants.js
@@ -1,4 +1,4 @@
-export const URL_TEMPLATE = '/static/media/tag-example.cfc481b5.docx'
+export const URL_TEMPLATE = '/tag-example.docx'
 export const MINE_TYPE = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
 export const DATE_FORMAT = 'YYYY_MM_DD_HH_mm_ss'
 export const TYPE_OUTPUT = 'blob'
