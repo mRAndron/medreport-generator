5ee21be5423d688e2354ce952e9d56317ab27736
jubasntc <jubas1337@mail.ru>
2019-07-27 23:04:31 +0300

    Create UI form

---
+++ public/setting-form.js
@@ -0,0 +1,27 @@
+export const doctorList = [
+  {
+    name: 'ALAN KHIGER', 
+    npi:  '1336452671',
+    signature: 'CH11222'
+  },
+  {
+    name: 'JACOB PLOTZ', 
+    npi:  '1114378973',
+    signature: 'CH11872'
+  },
+  {
+    name: 'T. GENTRY', 
+    npi:  '1891276556',
+    signature: 'CH12517'
+  }
+]
+
+export const insuranceList = [
+  { value: 'STATEFARM', label: 'STATEFARM' }
+]
+
+export const officeAddressList = [
+  '6320 ST. AUGUSTINE RD. #10, JACKSONVILLE, FL 32217',
+  '5233 RICKER RD., STE.102, JACKSONVILLE, FL 32210',
+  '2907 SPRING GLEN RD, JACKSONVILLE, FL 32207'
+]
