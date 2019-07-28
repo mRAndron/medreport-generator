82883f6281868a9244192f2e50bb0690f1f09d29
lulldev <lulldev@yandex.ru>
2019-07-28 16:00:35 +0300

    Updates

---
+++ precache-manifest.20b173bd21a140e482c98a2f851409f7.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc964110be710c13db28354cb46fef2c",
+    "url": "/medreport-generator/index.html"
+  },
+  {
+    "revision": "3756516c4c60cc8efb74",
+    "url": "/medreport-generator/static/css/2.72500db8.chunk.css"
+  },
+  {
+    "revision": "ddde9ccc1fe973d21e0e",
+    "url": "/medreport-generator/static/css/main.fcb22110.chunk.css"
+  },
+  {
+    "revision": "3756516c4c60cc8efb74",
+    "url": "/medreport-generator/static/js/2.70c05048.chunk.js"
+  },
+  {
+    "revision": "ddde9ccc1fe973d21e0e",
+    "url": "/medreport-generator/static/js/main.419ea097.chunk.js"
+  },
+  {
+    "revision": "562bfe434ddbe25cc64a",
+    "url": "/medreport-generator/static/js/runtime~main.b1c737d9.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/medreport-generator/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/medreport-generator/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/medreport-generator/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/medreport-generator/static/media/notification.c0d3c94c.eot"
+  }
+]);
---
+++ setting-form.js
@@ -0,0 +1,137 @@
+window.insuranceList = [
+  { value: 'STATEFARM', label: 'STATEFARM' }
+]
+
+window.doctorList = [
+  {
+    value:  {
+      name: 'ALAN KHIGER',
+      npi:  '1336452671',
+      signature: 'CH11222'
+    },
+    label: 'ALAN KHIGER'
+  },
+  {
+    value: {
+      name: 'JACOB PLOTZ',
+      npi:  '1114378973',
+      signature: 'CH11872'
+    },
+    label: 'JACOB PLOTZ'
+  },
+  {
+    value: {
+      name: 'T. GENTRY',
+      npi:  '1891276556',
+      signature: 'CH12517'
+    },
+    label: 'T. GENTRY'
+  }
+]
+
+window.officeAddressList = [
+  { 
+    value: '6320 ST. AUGUSTINE RD. #10, JACKSONVILLE, FL 32217',
+    label: '6320 ST. AUGUSTINE RD. #10, JACKSONVILLE, FL 32217'
+  },
+  {
+    value: '5233 RICKER RD., STE.102, JACKSONVILLE, FL 32210',
+    label: '5233 RICKER RD., STE.102, JACKSONVILLE, FL 32210'
+  },
+  {
+    value: '2907 SPRING GLEN RD, JACKSONVILLE, FL 32207',
+    label: '2907 SPRING GLEN RD, JACKSONVILLE, FL 32207'
+  }
+]
+
+
+window.servicesList = [
+  { 
+    value: { code: '97014', pay: '50' },
+    label: '97014'
+  }, 
+  { 
+    value: { code: '97010', pay: '50' },
+    label: '97010'
+  },
+  { 
+    value: { code: '97039', pay: '60' },
+    label: '97039'
+  },
+  {
+    value: { code: '97012', pay: '75' },
+    label: '97012'
+  },
+  {
+    value: { code: '97035', pay: '35' },
+    label: '97035'
+  },
+  { 
+    value: { code: '97110', pay: '70' },
+    label: '97110'
+  },
+  {
+    value: { code: '97140', pay: '60' },
+    label: '97140'
+  },
+  {
+    value: { code: '97530', pay: '70' },
+    label: '97530'
+  },
+  {
+    value: { code: '97112', pay: '' },
+    label: '97112'
+  },
+  {
+    value: { code: '97535', pay: '' },
+    label: '97535'
+  },
+  {
+    value: { code: '76140', pay: '85' },
+    label: '76140'
+  },
+  {
+    value: { code: '98940', pay: '80' },
+    label: '98940'
+  },
+  {
+    value: { code: '98941', pay: '100' },
+    label:'98941'
+  },
+  {
+    value: { code: '98942', pay: '120' },
+    label: '98942'
+  },
+  {
+    value: { code: '98943', pay: '80' },
+    label: '98943'
+  },
+  { 
+    value: { code: '99202', pay: '250' },
+    label: '99202'
+  },
+  {
+    value: { code: '99203', pay: '350' },
+    label: '99203'
+  },
+  {
+    value: { code: '99212', pay: '150' },
+    label: '99212'
+  },
+  {
+    value: { code: '99213', pay: '200' },
+    label: '99213'
+  },
+  {
+    value: { code: '99348', pay: '350' },
+    label: '99348'
+  },
+  {
+    value: { code: '99343', pay: '450' },
+    label: '99343'
+  }, 
+  {
+    value: { code: '99082', pay: '20' },
+    label: '99082'
+  }
+]
