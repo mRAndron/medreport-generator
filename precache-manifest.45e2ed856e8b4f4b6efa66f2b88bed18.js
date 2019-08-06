08c648364cb1e7c192c0dd91e168eb3d201f4cbc
lulldev <lulldev@yandex.ru>
2019-08-06 23:45:47 +0300

    Updates

---
+++ precache-manifest.45e2ed856e8b4f4b6efa66f2b88bed18.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c6a285520243fefe8877a2fa9c0ac945",
+    "url": "/medreport-generator/index.html"
+  },
+  {
+    "revision": "0a3fda5b0942e23127c8",
+    "url": "/medreport-generator/static/css/2.72500db8.chunk.css"
+  },
+  {
+    "revision": "5f2b433a690c7f7f38af",
+    "url": "/medreport-generator/static/css/main.a2a4d70e.chunk.css"
+  },
+  {
+    "revision": "0a3fda5b0942e23127c8",
+    "url": "/medreport-generator/static/js/2.25cc327a.chunk.js"
+  },
+  {
+    "revision": "5f2b433a690c7f7f38af",
+    "url": "/medreport-generator/static/js/main.d5fb79a2.chunk.js"
+  },
+  {
+    "revision": "562bfe434ddbe25cc64a",
+    "url": "/medreport-generator/static/js/runtime~main.b1c737d9.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/medreport-generator/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/medreport-generator/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/medreport-generator/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/medreport-generator/static/media/notification.c0d3c94c.eot"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/medreport-generator/precache-manifest.d939d44ce6ea72b379b1bda058874ac2.js"
+  "/medreport-generator/precache-manifest.45e2ed856e8b4f4b6efa66f2b88bed18.js"
 );
 
 self.addEventListener('message', (event) => {
