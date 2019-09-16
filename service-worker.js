e2ce4972eb381bda71bff46f8bfa79632c7667b2
lulldev <lulldev@yandex.ru>
2019-09-16 15:52:51 +0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/medreport-generator/precache-manifest.49c80b539ac0ff557e8f156fa71afe2f.js"
+  "/medreport-generator/precache-manifest.21ba4f60fa9eb641848ba28f27474d3a.js"
 );
 
 self.addEventListener('message', (event) => {
