1c3e28facdad83382ef55d82a3be94593909d8c7
JubasNTC <31780672+JubasNTC@users.noreply.github.com>
2019-11-17 12:28:46 +0300

    create page app template

--- src/pages/Generator/index.tsx
+++ src/pages/Generator/index.tsx
@@ -2,9 +2,8 @@ import * as React from 'react';
 import {Page} from '../../components/templates/Page';
 
 const Generator: React.FC = () => {
-  return (
-    <Page>
-      {}
+  return (<Page>
+{}
     </Page>
   );
 };
