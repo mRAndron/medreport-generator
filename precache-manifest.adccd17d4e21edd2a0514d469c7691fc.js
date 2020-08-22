50e562ec19382b2ecdaba8c89dc77194caa20c05
Andrey <67390642+mRAndron@users.noreply.github.com>
2020-08-22 22:36:04 +0300

    1

---
+++ precache-manifest.adccd17d4e21edd2a0514d469c7691fc.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dffaa1c57007e1230f1587935a30775a",
+    "url": "/medreport-generator/index.html"
+  },
+  {
+    "revision": "3e4a9a84a34d77938c6f",
+    "url": "/medreport-generator/static/css/2.72500db8.chunk.css"
+  },
+  {
+    "revision": "da6701a21bbaa1b97aa7",
+    "url": "/medreport-generator/static/css/main.2838fb0f.chunk.css"
+  },
+  {
+    "revision": "3e4a9a84a34d77938c6f",
+    "url": "/medreport-generator/static/js/2.9da5f8e2.chunk.js"
+  },
+  {
+    "revision": "da6701a21bbaa1b97aa7",
+    "url": "/medreport-generator/static/js/main.a91cca9e.chunk.js"
+  },
+  {
+    "revision": "562bfe434ddbe25cc64a",
+    "url": "/medreport-generator/static/js/runtime~main.b1c737d9.js"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/medreport-generator/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/medreport-generator/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/medreport-generator/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/medreport-generator/static/media/notification.c0d3c94c.eot"
+  }
+]);
